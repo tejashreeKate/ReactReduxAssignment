1549b6defd032cc9c12c09f9d4771290d5c3b9b0
tejashree kate <teju.kate@gmail.com>
2018-04-25 11:53:15 -0400

    Update userAction.js

--- src/actions/userAction.js
+++ src/actions/userAction.js
@@ -1,11 +1,16 @@
+const userInfo = {
+	uname:"test",
+	pwd:"test"
+}
+/** set user data in the store*/
 export function setUserInfo(data){
 	return {type:"SAVE_USER_INFO",payload:data}
 }
 
+/** validate user data with the static user info and return true if details match*/
 export function validateUserInfo(data){
-		if(data.username=="test" && data.password=="test"){
+		if(data.username==userInfo.uname && data.password==userInfo.pwd){
 			return true;
 		}
 	return false;
-	
 }
